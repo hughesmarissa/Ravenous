bc96e8234ae57873ab95c79d182fa252a8b6ee70
Marissa Hughes <hughes.marissa331@gmail.com>
2021-11-02 11:41:48 -0700

    Add Create SearchBar.js and styles

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -6,12 +6,12 @@ class BusinessList extends React.Component {
   render() {
     return(
       <div className="BusinessList">
-{/* <!-- Add Business Component here --> 
-<!-- Add Business Component here -->
-<!-- Add Business Component here -->
-<!-- Add Business Component here -->
-<!-- Add Business Component here -->
-<!-- Add Business Component here --> */}
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+        <Business />
       </div>
     )
   }
